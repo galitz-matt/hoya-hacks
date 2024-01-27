221fc342db7376a1ec7b64f2d0e2381521aea6fd
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2024-01-27 16:34:43 -0500

    extract Adam's scripts to js files

---
+++ js/index.js
@@ -0,0 +1,133 @@
+d3.csv('https://raw.githubusercontent.com/KhanradCoder/hoya-hacks/main/firstyearhousing_monthy.csv').then(function(data) {
+  //Get Scores for Specific House
+  var house_name = 'Courtnay';
+  var houseData = data.filter(function(d) { return d.House === house_name; });
+  const columnNames = data.columns
+  const months = columnNames.slice(2, columnNames.length - 2);
+  var electricity = houseData.find(function(d) { return d.Type === 'Electricity'; });
+  electricity = months.map(function(month) { 
+    return electricity && electricity[month] ? parseFloat(electricity[month].replace(/,/g, '')) : 0; 
+  });
+
+  var chilledWater = houseData.find(function(d) { return d.Type === 'Chilled Water'; });
+  chilledWater = months.map(function(month) { 
+    return chilledWater && chilledWater[month] ? parseFloat(chilledWater[month].replace(/,/g, '')) : 0; 
+  });
+
+  var mthw = houseData.find(function(d) { return d.Type === 'MTHW'; });
+  mthw = months.map(function(month) { 
+    return mthw && mthw[month] ? parseFloat(mthw[month].replace(/,/g, '')) : 0; 
+  });
+
+  var steam = houseData.find(function(d) { return d.Type === 'Steam'; });
+  steam = months.map(function(month) { 
+    return steam && steam[month] ? parseFloat(steam[month].replace(/,/g, '')) : 0; 
+  });
+
+  //Combination of steam and mthw
+  var heat = mthw.map(function(value, index) {
+    return value + (steam[index] || 0);
+  });
+
+  var naturalGas = houseData.find(function(d) { return d.Type === 'Natural Gas'; });
+  naturalGas = months.map(function(month) { 
+    return naturalGas && naturalGas[month] ? parseFloat(naturalGas[month].replace(/,/g, '')) : 0; 
+  });
+
+  var score_electric = electricity.map(function(value) {return (0.000259*value)});
+  var score_chilledWater = chilledWater.map(function(value) {return (0.017048*value)});
+  var score_hotWater = heat.map(function(value) {return (0.059224*value)});
+  var score_naturalGas = naturalGas.map(function(value) {return (0.053068*value)});
+  
+  //Chart
+  var ctx = document.getElementById('chart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: months,
+      datasets: [{
+        label: 'Electricity',
+        data: score_electric,
+        backgroundColor: 'rgb(190, 156, 146, 0.2)',
+        borderColor: 'rgb(190, 156, 146, 1)',
+        borderWidth: 1
+      },
+      {
+        label: 'Chilled Water',
+        data: score_chilledWater,
+        backgroundColor: 'rgb(146, 180, 190, 0.2)',
+        borderColor: 'rgb(146, 180, 190, 1)',
+        borderWidth: 1
+      },
+      {
+        label: 'Hot Water',
+        data: score_hotWater,
+        backgroundColor: 'rgb(178, 146, 190, 0.2)',
+        borderColor: 'rgb(178, 146, 190, 1)',
+        borderWidth: 1
+      },
+      {
+        label: 'Natural Gas',
+        data: score_naturalGas,
+        backgroundColor: 'rgb(146, 190, 156, 0.2)',
+        borderColor: 'rgb(146, 190, 156, 1)',
+        borderWidth: 1
+      }  
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: house_name+' House Environmental Impact',
+      },
+      scales: {
+        xAxes: [{
+          stacked: true
+        }],
+        yAxes: [{
+          stacked: true,
+          ticks: {
+            min: 0,
+            suggestedMax: 18
+          }
+        }]
+      }
+    }
+  });
+
+  //Get Rankings
+  var totalScores = {};
+  var electricScores = {};
+  var chilledWaterScores = {}; 
+  var hotWaterScores = {};
+  var lastMonth = months[months.length - 1];
+  data.forEach(element => {
+    var houseData = data.filter(function(item) { return item.House === element.House});
+
+    var electricity = houseData.find(function(d) { return d.Type === 'Electricity'; });
+    electricity = electricity && electricity[lastMonth] ? parseFloat(electricity[lastMonth].replace(/,/g, '')) : 0;
+
+    var chilledWater = houseData.find(function(d) { return d.Type === 'Chilled Water'; });
+    chilledWater = chilledWater && chilledWater[lastMonth] ? parseFloat(chilledWater[lastMonth].replace(/,/g, '')) : 0;
+
+    var mthw = houseData.find(function(d) { return d.Type === 'MTHW'; });
+    mthw = mthw && mthw[lastMonth] ? parseFloat(mthw[lastMonth].replace(/,/g, '')) : 0;
+
+    var steam = houseData.find(function(d) { return d.Type === 'Steam'; });
+    steam = steam && steam[lastMonth] ? parseFloat(steam[lastMonth].replace(/,/g, '')) : 0;
+
+    //Combination of steam and mthw
+    var hotWater = mthw + steam;
+
+    var total = electricity + chilledWater + hotWater;
+
+    totalScores[element.House] = total;
+    electricScores[element.House] = electricity;
+    chilledWaterScores[element.House] = chilledWater;
+    hotWaterScores[element.House] = hotWater;
+  });
+
+  var minTotalScore = Object.entries(totalScores).reduce((prev, curr) => prev[1] < curr[1] ? prev : curr)[0];
+  var minElectricScore = Object.entries(electricScores).reduce((prev, curr) => prev[1] < curr[1] ? prev : curr)[0];
+  var minChilledWaterScore = Object.entries(chilledWaterScores).reduce((prev, curr) => prev[1] < curr[1] ? prev : curr)[0];
+  var minHotWaterScore = Object.entries(hotWaterScores).reduce((prev, curr) => prev[1] < curr[1] ? prev : curr)[0];
--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,28 +1,98 @@
-const leaderboardData = [
-    { rank: 1, name: "Gibbons Dorm", score: 1500 },
-    { rank: 2, name: "Lyle-Maupin Dorm", score: 1000 },
-    { rank: 3, name: "McCormick Dorm", score: 500 },
-    // ... other entries ...
-];
+d3.csv('https://raw.githubusercontent.com/KhanradCoder/hoya-hacks/main/firstyearhousing_monthy.csv').then(function(data) {
+  const columnNames = data.columns
+  const months = columnNames.slice(2, columnNames.length - 2);
+  var totalScores = {};
+  var electricScores = {};
+  var chilledWaterScores = {}; 
+  var hotWaterScores = {};
+  var lastMonth = months[months.length - 1];
+  console.log(lastMonth);
+  data.forEach(element => {
+    var houseData = data.filter(function(item) { return item.House === element.House});
 
-// Function to populate the leaderboard table
-function populateLeaderboardTable(data) {
-    const table = document.querySelector(".leaderboard .leaderboard-table");
-    // Assuming the table is empty, add the header row first
-    let headerHtml = '<tr class="header-row"><th>Rank</th><th>Name</th><th>Score</th></tr>';
-    table.innerHTML = headerHtml; // Set the headers
+    var electricity = houseData.find(function(d) { return d.Type === 'Electricity'; });
+    var lastElectricity = electricity && electricity[lastMonth] ? parseFloat(electricity[lastMonth].replace(/,/g, '')) : 0;
 
-    // Populate the data rows
-    data.forEach(entry => {
-        let rowHtml = `<tr>
-                         <td>${entry.rank}</td>
-                         <td>${entry.name}</td>
-                         <td>${entry.score}</td>
-                       </tr>`;
-        table.innerHTML += rowHtml; // Append the rows
+    var chilledWater = houseData.find(function(d) { return d.Type === 'Chilled Water'; });
+    var lastChilledWater = chilledWater && chilledWater[lastMonth] ? parseFloat(chilledWater[lastMonth].replace(/,/g, '')) : 0;
+
+    var mthw = houseData.find(function(d) { return d.Type === 'MTHW'; });
+    var lastMTHW = mthw && mthw[lastMonth] ? parseFloat(mthw[lastMonth].replace(/,/g, '')) : 0;
+
+    var steam = houseData.find(function(d) { return d.Type === 'Steam'; });
+    lastSteam = steam && steam[lastMonth] ? parseFloat(steam[lastMonth].replace(/,/g, '')) : 0;
+
+    //Combination of steam and mthw
+    var hotWater = {};
+    var totalHotWater = 0;
+    var hotWaterValues = [];
+    months.forEach(function(month) {
+      var mthwValue = mthw && mthw[month] ? parseFloat(mthw[month].replace(/,/g, '')) : 0;
+      var steamValue = steam && steam[month] ? parseFloat(steam[month].replace(/,/g, '')) : 0;
+      hotWater[month] = mthwValue + steamValue;
+      totalHotWater += hotWater[month];
+      hotWaterValues.push(hotWater[month]);
     });
-}
+    var lastHotWater = lastMTHW + lastSteam;
 
-document.addEventListener('DOMContentLoaded', () => {
-    populateLeaderboardTable(leaderboardData);
+    var electricMean = parseFloat(electricity['Mean'].replace(/,/g, ''));
+    var electricStd = parseFloat(electricity['Standard Deviation'].replace(/,/g, ''));
+    var chilledWaterMean = chilledWater && chilledWater['Mean'] ? parseFloat(chilledWater['Mean'].replace(/,/g, '')) : 0;
+    var chilledWaterStd = chilledWater && chilledWater['Standard Deviation'] ? parseFloat(chilledWater['Standard Deviation'].replace(/,/g, '')) : 0;
+    var hotWaterMean = totalHotWater / months.length;
+    // Calculate hotWater variance
+    var hotWaterVariance = hotWaterValues.reduce(function(sum, value) {
+      return sum + Math.pow(value - hotWaterMean, 2);
+    }, 0) / hotWaterValues.length;
+    // Calculate hotWater standard deviation
+    var hotWaterStd = Math.sqrt(hotWaterVariance);
+
+    electricScores[element.House] = (lastElectricity - electricMean) / electricStd;
+    chilledWaterScores[element.House] = (lastChilledWater - chilledWaterMean) / chilledWaterStd;
+    if (isNaN(chilledWaterScores[element.House])) {
+      chilledWaterScores[element.House] = 0;
+    }
+    hotWaterScores[element.House] =  (lastHotWater - hotWaterMean) / hotWaterStd;
+    totalScores[element.House] = electricScores[element.House] + chilledWaterScores[element.House] + hotWaterScores[element.House];
+  });
+
+  var minTotalScore = Object.entries(totalScores).reduce((prev, curr) => prev[1] < curr[1] ? prev : curr, [null, Infinity]);
+  var minElectricScore = Object.entries(electricScores).reduce((prev, curr) => prev[1] < curr[1] ? prev : curr, [null, Infinity]);
+  var minChilledWaterScore = Object.entries(chilledWaterScores).reduce((prev, curr) => prev[1] < curr[1] ? prev : curr, [null, Infinity]);
+  var minHotWaterScore = Object.entries(hotWaterScores).reduce((prev, curr) => prev[1] < curr[1] ? prev : curr, [null, Infinity]);
+
+//   console.log(totalScores);
+//   console.log('Best Total Score:', minTotalScore[0], minTotalScore[1]);
+//   console.log('Best Electric Score:', minElectricScore[0], minElectricScore[1]);
+//   console.log('Best Chilled Water Score:', minChilledWaterScore[0], minChilledWaterScore[1]);
+//   console.log('Best Hot Water Score:', minHotWaterScore[0], minHotWaterScore[1]);
 });
+
+// const leaderboardData = [
+//     { rank: 1, name: "Gibbons Dorm", score: 1500 },
+//     { rank: 2, name: "Lyle-Maupin Dorm", score: 1000 },
+//     { rank: 3, name: "McCormick Dorm", score: 500 },
+//     // ... other entries ...
+// ];
+
+// // Function to populate the leaderboard table
+// function populateLeaderboardTable(data) {
+//     const table = document.querySelector(".leaderboard .leaderboard-table");
+//     // Assuming the table is empty, add the header row first
+//     let headerHtml = '<tr class="header-row"><th>Rank</th><th>Name</th><th>Score</th></tr>';
+//     table.innerHTML = headerHtml; // Set the headers
+
+//     // Populate the data rows
+//     data.forEach(entry => {
+//         let rowHtml = `<tr>
+//                          <td>${entry.rank}</td>
+//                          <td>${entry.name}</td>
+//                          <td>${entry.score}</td>
+//                        </tr>`;
+//         table.innerHTML += rowHtml; // Append the rows
+//     });
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     populateLeaderboardTable(leaderboardData);
+// });
